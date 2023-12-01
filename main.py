f6d921bc7f7df4fe3627aaf828d5d03d20bf9b24
k.lavkin <kirjanstart@bk.ru>
2023-12-01 18:48:45 +0300

    First version echo bot

---
+++ main.py
@@ -0,0 +1,30 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+
+logging.basicConfig(level=logging.INFO)
+API_TOKEN = '6861695078:AAEsMfeI4mbt_HreAEUmsVcAYV83ZrGdttM'
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command('start'))
+async def send_welcome(message: types.Message):
+    await message.reply(f'Привет!\nЯ Botty!\nОтправь мне любое сообщение и я на него отвечу!')
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+     asyncio.run(main())
+
+@dp.message(Command('stop'))
+async def stops():
+    await dp.stop_polling()
