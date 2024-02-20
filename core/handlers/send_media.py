2dd320f853c270e151e60f98f326d74ced3ec3bf
k.lavkin <kirjanstart@bk.ru>
2024-02-20 16:23:05 +0300

    Bot can send media files

---
+++ core/handlers/send_media.py
@@ -0,0 +1,50 @@
+from aiogram.types import Message, FSInputFile, InputMediaPhoto, InputMediaVideo
+from aiogram import Bot
+from aiogram.utils.chat_action import ChatActionSender
+
+
+async def get_audio(message: Message, bot: Bot):
+    audio = FSInputFile(path=r'C:\Users\k.lavkin\Desktop\Trava_u_doma.mp3', filename='audio1.mp3')
+    await  bot.send_audio(message.chat.id, audio=audio)
+
+
+async def get_document(message: Message, bot: Bot):
+    document = FSInputFile(path=r'C:\Users\k.lavkin\Desktop\Документ.docx')
+    await bot.send_document(message.chat.id, document=document, caption='Это док')
+
+
+async def get_media_group(message: Message, bot: Bot):
+    photo1_mg = InputMediaPhoto(type='photo', media=FSInputFile(r'C:\Users\k.lavkin\Desktop\1.jpg'),
+                               caption="It's media group")
+    photo2_mg = InputMediaPhoto(type='photo', media=FSInputFile(r'C:\Users\k.lavkin\Desktop\2.webp'))
+    video_mg = InputMediaVideo(type='video', media=FSInputFile(r'C:\Users\k.lavkin\Desktop\6.MP4'))
+    media = [photo2_mg, photo1_mg, video_mg]
+    await bot.send_media_group(message.chat.id, media)
+
+
+async def get_photo(message: Message, bot: Bot):
+    photo = FSInputFile(path=r'C:\Users\k.lavkin\Desktop\1.jpg')
+    await  bot.send_photo(message.chat.id, photo=photo, caption='Это фото')
+
+
+async def get_sticker(message: Message, bot: Bot):
+    sticker = FSInputFile(path=r'C:\Users\k.lavkin\Desktop\2.webp')
+    await  bot.send_sticker(message.chat.id, sticker=sticker)
+
+
+async def get_video(message: Message, bot: Bot):
+    async with ChatActionSender.upload_video(chat_id=message.chat.id, bot=bot):
+        video = FSInputFile(path=r'C:\Users\k.lavkin\Desktop\6.MP4')
+        await  bot.send_video(message.chat.id, video=video, caption='Это видео')
+
+
+async def get_video_note(message: Message, bot: Bot):
+    async with ChatActionSender.upload_video_note(chat_id=message.chat.id, bot=bot):
+        video_note = FSInputFile(path=r'C:\Users\k.lavkin\Desktop\6.MP4')
+        await  bot.send_video_note(message.chat.id, video_note=video_note)
+
+
+async def get_voice(message: Message, bot: Bot):
+    async with ChatActionSender.record_voice(chat_id=message.chat.id, bot=bot):
+        voice = FSInputFile(path=r'C:\Users\k.lavkin\Desktop\Trava_u_doma.opus')
+        await  bot.send_voice(message.chat.id, voice=voice, caption='Это голосовуха')
--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from core.utils.statesform import StepsForm
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from core.handlers import apsched
 from datetime import datetime, timedelta
+from core.handlers import send_media
 
 
 logging.basicConfig(level=logging.INFO)
@@ -47,25 +48,37 @@ async  def crete_pool():
 async def main():
     # dp.startup.register(start_bot)
     # dp.shutdown.register(stop_bot)
-    pool_connect = await crete_pool()
+    # pool_connect = await crete_pool()
     # pool_connect = await asyncpg.create_pool(user='postgres', password='3846', database='users',
     #                                         host= '127.0.0.1', port=5432, command_timeout=60)
-    scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
-    scheduler.add_job(apsched.send_message_time, trigger='date', run_date=datetime.now() + timedelta(seconds=10),
-                      kwargs={'bot': bot})
-    scheduler.add_job(apsched.send_message_cron, trigger='cron', hour=datetime.now().hour,
-                      minute=datetime.now().minute + 1, start_date=datetime.now(), kwargs={'bot': bot})
-    scheduler.add_job(apsched.send_message_interval, trigger='interval', seconds=60, kwargs={'bot': bot})
-    scheduler.start()
-    dp.update.middleware.register(DbSession(pool_connect))
-    dp.message.middleware.register(CounterMiddleware())
+    # scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
+    # scheduler.add_job(apsched.send_message_time, trigger='date', run_date=datetime.now() + timedelta(seconds=10),
+    #                   kwargs={'bot': bot})
+    # scheduler.add_job(apsched.send_message_cron, trigger='cron', hour=datetime.now().hour,
+    #                   minute=datetime.now().minute + 1, start_date=datetime.now(), kwargs={'bot': bot})
+    # scheduler.add_job(apsched.send_message_interval, trigger='interval', seconds=60, kwargs={'bot': bot})
+    # scheduler.start()
+
+    # dp.update.middleware.register(DbSession(pool_connect))
+    # dp.message.middleware.register(CounterMiddleware())
     # dp.message.middleware.register(OfficeHoursMiddleware())
-    dp.update.middleware.register(OfficeHoursMiddleware())
-    dp.update.middleware.register(SchedulerMiddleware(scheduler))
+    # dp.update.middleware.register(OfficeHoursMiddleware())
+    # dp.update.middleware.register(SchedulerMiddleware(scheduler))
+
+    dp.message.register(send_media.get_audio, Command(commands='audio'))
+    dp.message.register(send_media.get_document, Command(commands='document'))
+    dp.message.register(send_media.get_media_group, Command(commands='mediagroup'))
+    dp.message.register(send_media.get_photo, Command(commands='photo'))
+    dp.message.register(send_media.get_sticker, Command(commands='sticker'))
+    dp.message.register(send_media.get_video, Command(commands='video'))
+    dp.message.register(send_media.get_video_note, Command(commands='video_note'))
+    dp.message.register(send_media.get_voice, Command(commands='voice'))
+
     dp.message.register(form.get_form, Command(commands='form'))
     dp.message.register(form.get_name, StepsForm.GET_NAME)
     dp.message.register(form.get_last_name, StepsForm.GET_LAST_NAME)
     dp.message.register(form.get_age, StepsForm.GET_AGE)
+
     dp.message.register(order, Command(commands='pay'))
     dp.pre_checkout_query.register(pre_checkout_query)
     dp.message.register(successful_payment, F.successful_payment)
